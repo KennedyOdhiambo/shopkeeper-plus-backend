93ec365c1f87c00b840bfc30d3efa6e53e7c87b4
Kennedy Odhiambo <x.johnkennedy.x@gmail.com>
2024-07-23 19:02:31 +0300

    add customer entity

---
+++ src/main/java/com/kennedy/shopkeeper_plus/Customers/Customer.java
@@ -0,0 +1,41 @@
+package com.kennedy.shopkeeper_plus.Customers;
+
+import com.kennedy.shopkeeper_plus.Utils.EntityStatus;
+import com.kennedy.shopkeeper_plus.users.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "customer_id")
+	private UUID customerId;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Column(name = "customer_name", nullable = false, length = 256)
+	private String customerName;
+
+	@Column(name = "customer_contact", nullable = false, length = 256)
+	private String customerContact;
+
+	@Column(name = "kra_pin", length = 256)
+	private String kraPin;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "status")
+	private EntityStatus status = EntityStatus.ACTIVE;
+
+}
