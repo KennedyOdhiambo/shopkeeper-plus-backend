4eaab9f27cebbecf6364d1cf0871ad4d5b7b0e9b
Kennedy Odhiambo <x.johnkennedy.x@gmail.com>
2024-07-23 17:18:01 +0300

    initial commit

---
+++ src/main/java/com/kennedy/shopkeeper_plus/ShopkeeperPlusApplication.java
@@ -0,0 +1,13 @@
+package com.kennedy.shopkeeper_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopkeeperPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopkeeperPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kennedy/shopkeeper_plus/ShopkeeperPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennedy.shopkeeper_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkeeperPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
