613f7480434065bfa7e8acae2aacba66af270ad7
Kennedy Odhiambo <x.johnkennedy.x@gmail.com>
2024-08-01 20:44:11 +0300

    add category controllers

---
+++ src/main/java/com/kennedy/shopkeeper_plus/controllers/CategoriesController.java
@@ -0,0 +1,47 @@
+package com.kennedy.shopkeeper_plus.controllers;
+
+import com.kennedy.shopkeeper_plus.dto.ResponseDto;
+import com.kennedy.shopkeeper_plus.dto.category.NewCategoryDto;
+import com.kennedy.shopkeeper_plus.dto.category.UpdateCategoryDto;
+import com.kennedy.shopkeeper_plus.services.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoriesController {
+	private final CategoryService categoryService;
+
+	public CategoriesController(CategoryService categoryService) {
+		this.categoryService = categoryService;
+	}
+
+	@PostMapping("/create")
+	public ResponseDto createCategory(
+			@Valid @RequestBody NewCategoryDto newCategoryDto
+	) {
+		return categoryService.createCategory(newCategoryDto);
+	}
+
+	@PatchMapping("/update")
+	public ResponseDto updateCategory(
+			@Valid @RequestBody UpdateCategoryDto updateCategoryDto
+	) {
+		return categoryService.updateCategory(updateCategoryDto);
+	}
+
+	@GetMapping("/list")
+	public ResponseDto listCategories() {
+		return categoryService.listCategories();
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseDto deleteCategory(
+			@PathVariable("id") UUID id
+	) {
+		return categoryService.deleteCategory(id);
+	}
+
+}
